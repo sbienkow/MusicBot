00449d531641da89deb3999ad56312f113c83520
Szymon <szymonbb@vp.pl>
2018-09-05 18:06:39 +0200

    Fixed path issue in docker run script

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d --name MusicBot --mount "type=bind,src=$(dirname "$BASH_SOURCE")/config,dst=/usr/src/musicbot/config" musicbot
+docker run -d --name MusicBot --mount "type=bind,src=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/config,dst=/usr/src/musicbot/config" musicbot
